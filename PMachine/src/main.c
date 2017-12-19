bee995739939b3c43393b19054d1f5323bb78772
Yuchen Zhang <superupon@126.com>
2017-12-19 22:57:42 +0800

    Add LED_Conf

--- PMachine/src/main.c
+++ PMachine/src/main.c
@@ -8,11 +8,13 @@ int main( void )
   // Configure CLK to internal 16M clock
   Clk_conf();
   uart_conf();
+  LED_Conf();
   while(1)
   {
     UART1_SendByte('a');
     Delay(20000);
     Delay(20000);
+    Blink(5);
   }
   
   return 0;
