bf8045537ed5ca54a6deba9abc1a0df53a6f4504
Yuchen Zhang <superupon@126.com>
2017-12-19 21:33:46 +0800

    Format uart module

--- PMachine/src/uart.c
+++ PMachine/src/uart.c
@@ -1,119 +1,78 @@
-/******************** (C) COPYRIGHT  风驰iCreate嵌入式开发工作室 ***************************
-
- * 文件名  ：uart.c
- * 描述    ：串口通信配置函数库     
- * 实验平台：iCreate STM8开发板
- * 寄存器版本  ：V1.0.0
- * 作者    ：ling_guansheng  QQ：779814207
- * 博客    ：
- *修改时间 ：2011-12-20
-  iCreate STM8开发板硬件连接
+/***************************************************************************
+ * File Name: uart.c
+ * Description: Serial Port Functions
+ * Writer: superupon@126.com
+ * Revision: 2011-12-20
     |--------------------|
-    |  USART1_RX-PA4     |
-    |  USART1_TX-PA5     |
+    |  USART1_TX-PD5     |
+    |  USART1_RX-PD6     |
     |--------------------|
-
-****************************************************************************************/
+***************************************************************************/
 
 #include "uart.h"
+
 /**************************************************************************
- * 函数名：uart_conf
- * 描述  ：uart配置函数
- * 输入  ：无
- *
- * 输出  ：无
- * 返回  ：无 
- * 调用  ：外部调用 
+ * Fuction Name: uart_conf
+ * Description: Configure uart
  *************************************************************************/
 void uart_conf(void)
 {
-    unsigned int baud_div=0;
-    
-    UART1_CR1 = (0<<4)|(0<<3)|(0<<2)|(0<<1)|(0<<0); 
-    /*1位起始位 8位数据位 结束位由CR3设置 不使用奇偶校验 不使能奇偶校验中断*/
-    UART1_CR2 = (0<<7)|(0<<6)|(1<<5)|(0<<4)|(1<<3)|(1<<2); 
-    /*使能发送和接收 接收中断使能 禁止发送中断*/
-    UART1_CR3 = (0<<6)|(0<<4)|(0<<3); /*设置1位停止位 不使能SCLK*/        
-    UART1_CR5 = (0<<2)|(0<<1);     
-    /*使用智能卡模式需要设置的，基本上保持默认就行了 */ 
-    
-    /*设置波特率*/
-    baud_div =HSIClockFreq/BaudRate;  /*求出分频因子*/
-    UART1_BRR2 = baud_div & 0x0f;
-    UART1_BRR2 |= ((baud_div & 0xf000) >> 8);
-    UART1_BRR1 = ((baud_div & 0x0ff0) >> 4);    /*先给BRR2赋值 最后再设置BRR1*/
-    
-    UART1_CR1 |= (0<<5);         /*使能UART*/
-
+  unsigned int baud_div=0;
+  
+  UART1_CR1 = (0<<4)|(0<<3)|(0<<2)|(0<<1)|(0<<0); 
+  /*1 start bit, 8 data bit, stop bit set by CR3, no parity, disable parity interrupt*/
+  UART1_CR2 = (0<<7)|(0<<6)|(1<<5)|(0<<4)|(1<<3)|(1<<2); 
+  /*Enable send and receive, enable receive interrupt, disable send interrupt*/
+  UART1_CR3 = (0<<6)|(0<<4)|(0<<3); /*Set 1 stop bit, not enable SCLK*/        
+  UART1_CR5 = (0<<2)|(0<<1);     
+  /*Keep Default Setting*/
+  
+  /*Set baud rate*/
+  baud_div = HSIClockFreq / BaudRate;           /*Calculate frequency division factor*/
+  UART1_BRR2 = baud_div & 0x0f;
+  UART1_BRR2 |= ((baud_div & 0xf000) >> 8);
+  UART1_BRR1 = ((baud_div & 0x0ff0) >> 4);    /*Set BRR2 first, then BRR1*/
+  
+  UART1_CR1 |= (0<<5);                        /*Enable UART*/
 }
+
 /**************************************************************************
- * 函数名：UART1_SendByte
- * 描述  ：uart发送一个字节
- * 输入  ：u8 data
- *
- * 输出  ：无
- * 返回  ：无 
- * 调用  ：外部调用 
- * 举例  ：UART1_SendByte('a')
+ * Function Name: UART1_SendByte
+ * Description ：uart port will send a byte
+ * Input  ：u8 data
+ * Example: UART1_SendByte('a')
  *************************************************************************/
 void UART1_SendByte(u8 data)
 {
-    
-    UART1_DR=data;
-   /* Loop until the end of transmission */
-   while (!(UART1_SR & UART1_FLAG_TXE));
+  UART1_DR = data;
+  /* Loop until the end of transmission */
+  while (!(UART1_SR & UART1_FLAG_TXE));
 }
 
 /**********************************************************************************
- * 函数名：UART1_SendByte
- * 描述  ：uart发送字符串
- * 输入  ：u8* Data,u16 len
- *
- * 输出  ：无
- * 返回  ：无 
- * 调用  ：外部调用 
- * 举例  ：UART1_SendString("iCreate STM8开发板",sizeof("iCreate STM8开发板"))
+ * Function Name: UART1_SendByte
+ * Description: Use uart to send string 
+ * Input: u8* Data,u16 len
+ * Example: UART1_SendString("iCreate STM8",sizeof("iCreate STM8"))
  **********************************************************************************/
 void UART1_SendString(u8* Data,u16 len)
 {
-  u16 i=0;
-  for(;i<len;i++)
+  u16 i = 0;
+  for (; i < len; i++)
     UART1_SendByte(Data[i]);
-  
 }
+
 /**********************************************************************************
- * 函数名：UART1_ReceiveByte
- * 描述  ：uart查询接收一个字节
- * 输入  ：无
- *
- * 输出  ：无
- * 返回  ：一个字节 
- * 调用  ：外部调用 
- * 举例  ：temp=UART1_ReceiveByte()
+ * Function Name: UART1_ReceiveByte
+ * Description: uart receive a byte
+ * Return: return a byte data
+ * Example: temp=UART1_ReceiveByte()
  **********************************************************************************/
 u8 UART1_ReceiveByte(void)
 {
-     u8 USART1_RX_BUF; 
-     while (!(UART1_SR & UART1_FLAG_RXNE));
-     USART1_RX_BUF=(uint8_t)UART1_DR;
-     return  USART1_RX_BUF;
-    
+  u8 USART1_RX_BUF;
+  while (!(UART1_SR & UART1_FLAG_RXNE))
+    ;
+  USART1_RX_BUF = (uint8_t)UART1_DR;
+  return USART1_RX_BUF;
 }
-
-
-/***********************************************
- * 函数名：fputc
- * 描述  ：重定向c库函数printf到USART1
- * 输入  ：无
- * 输出  ：无
- * 调用  ：由printf调用
- ***********************************************/
-//int fputc(int ch, FILE *f)
-//{  
- /*将Printf内容发往串口*/ 
-  //UART1_DR=(unsigned char)ch;
-  //while (!(UART1_SR & UART1_FLAG_TXE));
-  //return (ch);
-//}
-/******************* (C) COPYRIGHT 风驰iCreate嵌入式开发工作室 *****END OF FILE****/
-
