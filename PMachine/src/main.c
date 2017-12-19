cf3b8755766a89990aa9ea4d4149bce8b80c0395
Yuchen Zhang <superupon@126.com>
2017-12-19 21:03:27 +0800

    Use UART1 to send test code

--- PMachine/src/main.c
+++ PMachine/src/main.c
@@ -1,12 +1,19 @@
 #include "clk_conf.h"
 #include <iostm8s103f3.h>
 #include "led.h"
-
+#include "uart.h"
 
 int main( void )
 {
   // Configure CLK to internal 16M clock
   Clk_conf();
+  uart_conf();
+  while(1)
+  {
+    UART1_SendByte('a');
+    Delay(20000);
+    Delay(20000);
+  }
   
   return 0;
 }
