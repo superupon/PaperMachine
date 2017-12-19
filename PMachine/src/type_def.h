f5727c42977219d0b219fb48fba6e57a2b4818ec
Yuchen Zhang <superupon@126.com>
2017-12-19 21:42:29 +0800

    Format type definition part

--- PMachine/src/type_def.h
+++ PMachine/src/type_def.h
@@ -1,24 +1,22 @@
-/******************** (C) COPYRIGHT  风驰iCreate嵌入式开发工作室 ********************
- * 文件名  ：type_def.h
- * 描述    ：风驰iCreate STM8开发板专用头文件 
- * 实验平台：iCreate STM8开发板
- * 寄存器版本  ：V1.0.0
- * 作者    ：ling_guansheng  QQ：779814207
- * 博客    ：
- *修改时间 ：2011-12-20
+/********************************************************************************
+ * File Name: type_def.h
+ * Author：superupon@126.com
+ * Revision: 
 **********************************************************************************/
 #ifndef __type_def_h
 #define __type_def_h
-/*主控芯片的头文件*/
+
+/*Header file for control chip*/
 #include "iostm8s103f3.h"
-/*bool 类型头文件*/
+/*bool header file*/
 #include "stdbool.h"
-/*总中断头文件*/
+/*Interrupt file*/
 #include "intrinsics.h"
-/*常用数据类型定义*/
+/*Usually Data type definition*/
 typedef unsigned char     uint8_t;
 typedef unsigned short    uint16_t;
 typedef unsigned long     uint32_t;
+
 #define FlagStatus     bool
 #define u8  uint8_t
 #define u16 uint16_t
@@ -26,5 +24,3 @@ typedef unsigned long     uint32_t;
 #define EnableInterrupt  __enable_interrupt() 
 #endif
 
-
-/******************* (C) COPYRIGHT 风驰iCreate嵌入式开发工作室 *****END OF FILE****/
