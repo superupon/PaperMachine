9d1a0f46c0efbb94b059ca54eb4dd2491f0a971c
Yuchen Zhang <superupon@126.com>
2018-01-15 19:47:41 +0800

    <dev>Add logic to determine output signal or not</dev>

--- PMachine/src/main.c
+++ PMachine/src/main.c
@@ -3,50 +3,73 @@
 #include "led.h"
 #include "uart.h"
 #include "gprs.h"
+#include "io.h"
 
-int send_command()
-{
-  Clear_ReceiveBuff();
-  UART1_SendString("AT", strlen("AT"));
-  UART1_SendByte(0x0D);
-  Delay(50000);
-  return Find_Recv_Str("OK");
-}
 int main( void )
 {
   int i = 0;
+  u8 index = 0;
+  int ret = 0;
+  char end_char[2];
+  int counter = 0;
+
+  end_char[0] = 0x1A; //End Character
+  end_char[1] = '\0';
+
   // Configure CLK to internal 16M clock
   Clk_conf();
   uart_conf();
   LED_Conf();
+  Output_Conf();
   EnableInterrupt;
+
+
   while(1)
   {
-    i = Check_Status();
-    //Clear_ReceiveBuff();
-    //UART1_SendByte(0x41);
-    //UART1_SendByte(0x54);
-    //UART1_SendByte(0x47);
-    //UART1_SendByte(0x0D);
-    //UART1_SendByte(0x0A);
-    //UART1_SendByte('|');
-    //UART1_SendString("AT", strlen("AT"));
-    //UART1_SendByte(0x0D);
-    //Delay(500000);
-    //Delay(600000);
-    //i = Find_Recv_Str("OK");
-    //i = send_command();
+    if(!i)
+      i = Check_Status();
+
     if(i)
     {
-      Blink(1);
-      send_data_to_server("\"115.159.154.49\",3000","hello,GA6 tcp test!");//发送数据到服务器
+      if (counter == 50)
+      {
+        ret = send_data_to_server("\"115.159.154.49\",3000", phone_str);//发送数据到服务器
+        // Get Phone Number from SIM CARD
+        Get_PhoneNumber();
+        counter = 0;
+      }
+
+      ret = send_at_command("AT+CIPSTATUS", "CONNECT OK", 50 * 2);
+      if(ret)
+      {
+        send_at_command("AT+CIPSEND", ">", 50); 
+        UART1_SendString("b",1);
+        send_at_command_end(end_char, "SEND OK", 50);
+        Delay(50000);
+      }
+      ret = Find_Recv_Str("ONE");
+      if(ret)
+      {
+        Blink(1);
+        Output_Low();
+        Delay(200000);
+        Output_High();
+      }
+
+      ret = Find_Recv_Str("THREE");
+      if(ret)
+      {
+        for(index = 0; index < 3; index++)
+        {
+          Output_Low();
+          Delay(200000);
+          Output_High();
+          Delay(600000);
+        }
+      }
     }
-    else
-    {
-      Blink(5);
-    }
-    //UART1_SendString(RxBuffer, UART_RX_NUM & 0x3F);
+    counter++;
   }
-  
+
   return 0;
 }
