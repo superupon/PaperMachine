a642789b50b92da76edf270e255f791ab8027afd
superupon <superupon@126.com>
2018-01-16 21:13:40 +0800

    <dev>Add socket part code to handle communication with device</dev>

--- server/app.js
+++ server/app.js
@@ -5,6 +5,10 @@ const response = require('./middlewares/response')
 const bodyParser = require('koa-bodyparser')
 const config = require('./config')
 
+var net = require('net')
+var HOST = '0.0.0.0'
+var PORT = 3000
+var socket = 0
 // 使用响应处理中间件
 app.use(response)
 
@@ -16,4 +20,25 @@ const router = require('./routes')
 app.use(router.routes())
 
 // 启动程序，监听端口
+console.info("haha")
 app.listen(config.port, () => debug(`listening on port ${config.port}`))
+
+net.createServer(function(sock){
+  console.log('connected')
+  socket = sock
+  sock.on('close', function(data){
+    console.log('closed')
+  })
+  sock.on('data', function(data){
+    console.log(data.toString())
+    //sock.write('ONE')
+  })
+}).listen(PORT, HOST)
+
+var timer = setInterval(log, 10000)
+function log()
+{
+  console.log('timer');
+  if(socket)
+    socket.write('ONE');
+}
