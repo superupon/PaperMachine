c502f3c27cf1b2b1da627e2bb8fa4b6135a442ee
superupon <superupon@126.com>
2018-01-16 21:14:45 +0800

    Change configuration according to our new cloud server.

--- server/config.js
+++ server/config.js
@@ -17,11 +17,11 @@ const CONF = {
      * 开发环境下，MySQL 的初始密码为您的微信小程序 appid
      */
     mysql: {
-        host: 'localhost',
+        host: '172.17.0.8',
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: 'wxd075164ff8e3bdb0',
+        pass: 'BBBBBEFHabek1z',
         char: 'utf8mb4'
     },
 
@@ -39,7 +39,14 @@ const CONF = {
 
     // 微信登录态有效期
     wxLoginExpires: 7200,
-    wxMessageToken: 'abcdefgh'
+    serverHost: '46937168.ziyid.club',
+    tunnelServerUrl: 'http://tunnel.ws.qcloud.la',
+    tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
+    // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.cloud.tencent.com/capi
+    qcloudAppId: '1255637151',
+    qcloudSecretId: 'AKIDoUyDF4my1oLdjastBD0XrxoPjnb9tTHy',
+    qcloudSecretKey: 'EG3bx72i8IDsJYbR3RcaLtFNI0UkncKq',
+    wxMessageToken: 'bcdefgh'
 }
 
 module.exports = CONF
