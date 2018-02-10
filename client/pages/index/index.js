342fa1e59fa45d6487aa33f66be5ac3f6611c342
Yuchen Zhang <superupon@126.com>
2018-02-10 23:08:49 +0800

    Make login to be called in onLoad function

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -13,7 +13,12 @@ Page({
         imageSrc:'River.jpg'
     },
     
-
+    /**
+   * 生命周期函数--监听页面加载
+   */
+    onLoad: function (options) {
+      this.login()
+    },
     // 用户登录示例
     login: function() {
         if (this.data.logged) return
