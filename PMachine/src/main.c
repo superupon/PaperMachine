2314e561deb2dadd412490ebec32612fa6e127a8
Yuchen Zhang <superupon@126.com>
2018-03-07 23:24:46 +0800

    Remove response to outer loop

--- PMachine/src/main.c
+++ PMachine/src/main.c
@@ -12,7 +12,9 @@ int main( void )
   int ret = 0;
   char end_char[2];
   int counter = 100;
+  int close_counter = 0;
   u8 response_flag = 0;
+  u8 response_3_flag = 0;
 
   end_char[0] = 0x1A; //End Character
   end_char[1] = '\0';
@@ -22,11 +24,12 @@ int main( void )
   uart_conf();
   LED_Conf();
   Output_Conf();
+  Output_High();
   EnableInterrupt;
 
 
   while(1)
-  {
+  {    
     if(!i)
       i = Check_Status();
     
@@ -40,12 +43,31 @@ int main( void )
       Delay(300000);
     }
     
+    if(response_3_flag)
+    {
+      response_3_flag = 0;
+      Blink(1);
+      for(index = 0 ; index < 3; index++)
+      {
+        Output_Low();
+        Delay(300000);
+        Output_High();
+        Delay(600000);
+      }   
+    }
+    
     if(i)
     {
       if (counter == 2000)
       {
         // Get Phone Number from SIM CARD
         Get_PhoneNumber();
+        if(close_counter == 100)
+        {
+           send_at_command("AT+CIPCLOSE", "OK", 50 * 2);
+           close_counter = 0;
+        }
+        close_counter++;
         ret = send_data_to_server("\"www.jiqizhixing.cn\",3000", phone_str);//发送数据到服务器
         counter = 0;
       }
@@ -61,17 +83,18 @@ int main( void )
       if(ret)
       {
         Clear_ReceiveBuff();
-        for(index = 0; index < 3; index++)
-        {
-          Output_Low();
-          Delay(200000);
-          Output_High();
-          Delay(600000);
-        }
+        response_3_flag = 1;     
+      }
+      
+      ret = Find_Recv_Str("TWO");
+      if(ret)
+      {
+        Clear_ReceiveBuff();   
       }
       Delay(1000);
     }
     counter++;
+    Output_High();
   }
 
   return 0;
